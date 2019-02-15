1bb7ec935c2b5a24c387bb66388374880fd32aa7
bobmatyas <ballium@protonmail.com>
2019-02-15 12:37:38 -0500

    added combat function + initialization sequence

--- main.js
+++ main.js
@@ -10,84 +10,97 @@ let defeatMessageEnding = "time";
 let userName;
 
 
+/* CALL THE FUNCTION TO START THE GAME */
+
+startGame();
+
+
+
+
 /* START THE GAME */
 
 function startGame() {
   let userPlay = prompt("Do you want to play?", "yes or no");
   if (userPlay.toLowerCase() === "yes") {
     userName = prompt("Enter your name to start the battle:");
+    startCombat();
   } else {
     console.log(`Refresh the page and enter your name when you are ready to play`);
   }
-  //console.log(userName);
 }
 
-//startGame();
 
-
-/* DAMAGE FUNCTION */
+/* GET RANDOM DAMAGE FUNCTION */
 
 function getDamage() {
   let randomHit = Math.floor(Math.random() * 5 + 1);
   return randomHit;
 }
 
-console.log(getDamage());
 
-//   /* START BATTLE SEQUENCE */
+/* START BATTLE SEQUENCE */
 
-//   while (userHealthPoints > 0 && grantHealthPoints > 0 && grantLivesLeft >= 1) {
+function startCombat() {
 
-//     let hitPoints = [1, 2];
-//     let randomHitUser = hitPoints[Math.floor(Math.random() * hitPoints.length)];
-//     let randomHitGrant = hitPoints[Math.floor(Math.random() * hitPoints.length)];
+  while (userHealthPoints > 0 && grantHealthPoints > 0 && grantLivesLeft >= 1) {
 
-//     userHealthPoints = userHealthPoints - randomHitUser;
-//     grantHealthPoints = grantHealthPoints - randomHitGrant;
+    /* Chose to attack or quit */
 
-//     if (userHealthPoints > 0) {
-//       console.log(`${userName} has ${userHealthPoints} health left.`);
-//     } else if (userHealthPoints <= 0) {
-//       console.log(
-//         `
-//     You have been dealt a fatal blow.
-    
-//     Grant WINS!
-//     `);
-//       break;
-//     }
+    let attackOrQuit = prompt("Attack or Quit?");
 
-//     if (grantHealthPoints > 0) {
-//       console.log(`Grant the Mighty Chicken has ${grantHealthPoints} health left.`);
-//     } else if (grantHealthPoints <= 0) {
-//       console.log(`Grant has been dealt a fatal blow!`);
-//       if (grantLivesLeft >= 1) {
-//         grantLivesLeft -= 1;
-//         grantHealthPoints = 10;
-//         grantDefeatCount += 1;
-//         if (grantDefeatCount > 1) {
-//           defeatMessageEnding = "times";
-//         }
-//       }
+    /* If attack, run through attack sequence */
 
-//       if (grantDefeatCount <= 2) {
-//         console.log(
-//           `
-//         Grant the Mighty Chicken has been defeated ${grantDefeatCount} ${defeatMessageEnding}.
-        
-//         Grant has come back to life!
-//         `
-//         );
-
-//       } else if (grantDefeatCount === 3) {
-//         console.log(
-//           `
-//         Grant the Mighty Chicken has been defeated 3 times!!! 
-        
-//         YOU WIN!
-//         `
-//         );
-//       }
-//     }
-//   }
-
+    if (attackOrQuit.toLowerCase() === "attack") {
+      
+      userHealthPoints = userHealthPoints - getDamage();
+      grantHealthPoints = grantHealthPoints - getDamage();
+  
+      if (userHealthPoints > 0) {
+        console.log(`${userName} has ${userHealthPoints} health left.`);
+      } else if (userHealthPoints <= 0) {
+        console.log(
+          `
+      You have been dealt a fatal blow.
+      
+      Grant WINS!
+      `);
+        break;
+      }
+  
+      if (grantHealthPoints > 0) {
+        console.log(`Grant the Mighty Chicken has ${grantHealthPoints} health left.`);
+      } else if (grantHealthPoints <= 0) {
+        console.log(`Grant has been dealt a fatal blow!`);
+        if (grantLivesLeft >= 1) {
+          grantLivesLeft -= 1;
+          grantHealthPoints = 10;
+          grantDefeatCount += 1;
+          if (grantDefeatCount > 1) {
+            defeatMessageEnding = "times";
+          }
+        }
+  
+        if (grantDefeatCount <= 2) {
+          console.log(
+            `
+          Grant the Mighty Chicken has been defeated ${grantDefeatCount} ${defeatMessageEnding}.
+          
+          Unfortunately for you, Grant has come back to life!
+          `
+          );
+        } else if (grantDefeatCount === 3) {
+          console.log(
+            `
+          Grant the Mighty Chicken has been defeated 3 times!!! 
+          
+          YOU WIN!
+          `
+          );
+        }
+      }  
+    } else if (attackOrQuit.toLowerCase() === "quit") {
+      console.log(`You have left the battle field. Come back when you are ready to fight Grant!`);
+      break;
+    }
+  }
+}
